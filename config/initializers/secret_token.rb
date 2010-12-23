685c6994b69756a7a9ea91e14ad17da41b278cf5
Chris Beer <chris@cbeer.info>
2010-12-23 15:24:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroservicesOnRails::Application.config.secret_token = '79a48e986f15e4e2ffdc855ebdcf9f3f3703cfa93ebf3f80c0b6e449654950ee4209de89a38eacdbccb5e165f012a97e6abd32e069a225ab21277b1f11017d8c'

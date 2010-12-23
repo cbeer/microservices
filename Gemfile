4a42a416c6176565709d83a790a06fbb388c82a0
Chris Beer <chris@cbeer.info>
2010-12-23 18:02:38 -0500

    add lockit support to Package

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 gem "noid"
 gem "bagit"
+gem "lockit"
 gem "validatable"
 
 # Use unicorn as the web server
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,11 +1,16 @@
 class Package < ActiveRecord::Base
   include Noid::ActiveRecord::Provider
+  include LockIt::Mixin
   identifier_field :identifier
   minter Noid::Persistence::JSON.new(:template => 's.sdd', :filename => persistence_path )
 
   has_many :assets, :as => :attachable, :dependent => :destroy
 
   after_commit :bag_manifest
+
+  def path
+    self.object_path
+  end
 
   def bag
     BagIt::Bag.new(object_path)
